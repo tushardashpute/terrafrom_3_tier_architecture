f3541360547be6f80381f4e31076fa3d593c15f7
unknown <tdashpute@gmail.com>
2020-12-06 14:12:47 +0530

    Added ELB code, pointing to the public EC2 instances

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable "public_cidr" {
     default = "10.0.1.0/24"
 }
 
+variable "public_cidr1" {
+    default = "10.0.2.0/24"
+}
+
 variable "private_cidr" { 
   default =  "10.0.3.0/24"
 }
--- vpc.tf
+++ vpc.tf
@@ -68,6 +68,17 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_subnet" "public_subnet1" {
+  cidr_block              = var.public_cidr1
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone1
+
+  tags = {
+    Name = "my-test-public-subnet1"
+  }
+}
+
 # Private Subnet
 resource "aws_subnet" "private_subnet" {
   cidr_block        = var.private_cidr
@@ -96,6 +107,12 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   route_table_id = aws_route_table.public_route.id
   subnet_id      = aws_subnet.public_subnet.id
   depends_on     = [aws_route_table.public_route, aws_subnet.public_subnet]
+}
+
+resource "aws_route_table_association" "public_subnet1_assoc" {
+  route_table_id = aws_route_table.public_route.id
+  subnet_id      = aws_subnet.public_subnet1.id
+  depends_on     = [aws_route_table.public_route, aws_subnet.public_subnet1]
 }
 
 # Associate Private Subnet with Private Route Table
@@ -206,5 +223,61 @@ resource "aws_db_instance" "rds" {
   publicly_accessible     = true
   storage_encrypted       = false
   vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  vpc_id = aws_vpc.main.id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+### Createion of ELB
+
+resource "aws_elb" "elb1" {
+  name = "terraform-elb"
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+  security_groups = ["${aws_security_group.elb.id}"]
+  #availability_zones = [aws_subnet.public_subnet.availability_zone , aws_subnet.public_subnet1.availability_zone]
+  subnets = [aws_subnet.public_subnet.id , aws_subnet.public_subnet1.id]
+  instances = [aws_instance.public-instance.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+	healthy_threshold = 2
+	unhealthy_threshold = 2
+	timeout = 3
+	target = "HTTP:80/"
+	interval = 30
+  }
+
+  tags = {
+	Name = "terraform-elb"
+  }
+
+}
+
+output "elb-dns" {
+  value = "${aws_elb.elb1.dns_name}"
 }
 
